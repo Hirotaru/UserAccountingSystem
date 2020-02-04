ffa1c11ee80c8f55aed8878d3b3c39f8787ff5bf
Андрей Ханин <tiago0@ya.ru>
2020-02-04 21:50:37 +0300

    Added Create page

--- UserAccountingSystem/Controllers/HomeController.cs
+++ UserAccountingSystem/Controllers/HomeController.cs
@@ -4,16 +4,31 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using UserAccountingSystem.DAL;
+using UserAccountingSystem.Models;
 
 namespace UserAccountingSystem.Controllers
 {
     public class HomeController : Controller
     {
         private UserAccountingContext db = new UserAccountingContext();
-        public ActionResult UserList()
+        public ActionResult Index()
         {
             var users = db.Users.ToList();
             return View(users);
+        }
+
+        public ActionResult CreateUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateUser(User user)
+        {
+            db.Users.Add(user);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
         }
 
     }
